0e7a508a2d632ebe76b34988b332a1bb79a0efb1
Jared S <jaredjames2020@gmail.com>
2019-10-13 21:21:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,14 +14,14 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    file_name = filename.split(' - ')
-    new_song = self.new(file_name[1])
+    @file_name = filename.split(' - ')
+    new_song = self.new(@file_name[1])
     new_song.artist = artist_name
     new_song
   end
 
   def artist_name
-    Artist.find_or_create_by_name
+    Artist.find_or_create_by_name(@filename[0])
   
   end
 
