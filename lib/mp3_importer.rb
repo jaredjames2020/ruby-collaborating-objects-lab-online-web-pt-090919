51e69da2ba1723426a256584eb53187c3f279e34
Jared S <jaredjames2020@gmail.com>
2019-10-13 21:35:57 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,13 @@
 class MP3Importer
   
-  attr_accessor :filepath
+  attr_accessor :path
 
-   def initialize(filepath)
-      @filepath = filepath
+   def initialize(path)
+      @path = path
    end
    
    def files
-     
+     Dir.entries(@path).select {|f| !File.directory? f}
    end
    
    def import
