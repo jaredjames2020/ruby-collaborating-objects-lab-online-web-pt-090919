2924c6d90158a36b111e53486664e63486105950
Jared S <jaredjames2020@gmail.com>
2019-10-13 20:27:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,22 @@ class Artist
     @@all
   end
 
+  def songs
+    Songs.all.select {|song| song.artist == self}
+  end
+  
+  def add_song
+    Song.artist
+  end
 
+  def self.find_or_create_by_name
+  
+  
+  end
+  
+  def print_songs
+    songs
+  end
 
 
 
