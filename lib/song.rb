6dbf80cab110bea7da9e0c534677f36a4abd8ea9
Jared S <jaredjames2020@gmail.com>
2019-10-13 20:31:09 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name, :artist, :artist_name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(filename)
+    new_song = Song.new(filename)
+    new_song.artist = artist_name
+  
+  end
+
+
+end
